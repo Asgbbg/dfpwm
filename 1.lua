5c910cd3d4137b480c87c125e3445f4727eca813
Asgbbg <64193995+Asgbbg@users.noreply.github.com>
2022-02-03 14:14:17 +0800

    Update 1.lua
    multiple speakers support

--- 1.lua
+++ 1.lua
@@ -1,11 +1,12 @@
 local dfpwm = require("cc.audio.dfpwm")
-local speaker = peripheral.find("speaker")
+local speakers = table.pack(peripheral.find("speaker"))
 
 local decoder = dfpwm.make_decoder()
-    for chunk in io.lines("Broken Arrows.dfpwm",16*1024) do
+    for chunk in io.lines("./disk2/3.dfpwm",16*1024) do
         local buffer = decoder(chunk)
-        
-        while not speaker.playAudio(buffer) do
+       for i = 1,speakers.n do 
+        while not speakers[i].playAudio(buffer) do
             os.pullEvent("speaker_audio_empty")
+        end
         end
 end
